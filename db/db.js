9e9b6d4c38fecf1ed5059dadebd6c849b74dcdef
Ahana4Banerjee <banerjeeahana4@gmail.com>
2025-08-10 00:11:23 +0530

    initial commit

--- db/db.js
+++ db/db.js
@@ -6,8 +6,8 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
